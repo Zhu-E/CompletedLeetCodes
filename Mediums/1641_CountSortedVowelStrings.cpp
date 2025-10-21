8b2a89b5354ad61c40d7e342dd14d6c691df957e
Edward Zhu <96358920+Zhu-E@users.noreply.github.com>
2025-10-21 16:03:00 -0700

    Create 1641_CountSortedVowelStrings.cpp

---
+++ Mediums/1641_CountSortedVowelStrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        vector<vector<int>> dp(n, vector<int>(5, 0));
+        dp[0][0]=1;
+        dp[0][1]=1;
+        dp[0][2]=1;
+        dp[0][3]=1;
+        dp[0][4]=1;
+        for(int i = 1; i < n; ++i){
+            for(int j = 0; j < 5; ++j){
+                for(int k = 0; k <= j; ++k){
+                    dp[i][j]+=dp[i-1][k];
+                }
+            }
+        }
+        return dp[n-1][0]+dp[n-1][1]+dp[n-1][2]+dp[n-1][3]+dp[n-1][4];
+    }
+};
